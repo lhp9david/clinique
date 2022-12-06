36a3cc42a4a19cf02a368fbc180c4337ee984bad
lhp9-tg <thomas.gouel@gmail.com>
2022-12-06 12:26:58 +0100

    Navbar ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,84 @@
-// JSON
+// Choix du Tab
 
-fetch('assets/json/store.json')
-    .then(response => response.json())
-    .then((data) => {
-        let db = data;
-        console.log(db.items);
+const tabs = document.querySelectorAll('.nav-link')
 
-        db.items.forEach(element => {
+window.addEventListener('click', event => {
 
-            let id = element.id;
-            let name = element.name;
-            let category = element.category;
-            let image = element.image;
-            let overview = element.overview;
-            let price = element.price;
+    if (event.target.classList.contains('nav-link') || event.target.classList.contains('logo')) {
+        if (event.target.classList.contains('nav-link')) { 
+            event.target.classList.add('active');
+            event.target.removeAttribute('href');
+            document.querySelector('.tab').id = event.target.classList[1]
+            tabs.forEach(element => {
+                if (element != event.target) {
+                    element.classList.remove('active');
+                    element.setAttribute('href', '#')
+                }
+        });
+        }
+        else {
+            document.querySelector('.tab').id = 'landing';
+            tabs.forEach(element => {
+                if (element != event.target) {
+                    element.classList.remove('active');
+                    element.setAttribute('href', '#')
+                }
+            });
+        }
+    }
+});
 
-            let anchor = document.createElement('div');
-            anchor.innerHTML = '<div class="card-body" id='+id+'>';
-            
-            if (category == 'Pokeballs') {
+
+    // fetch('assets/json/store.json')
+    //     .then(response => response.json())
+    //     .then((data) => {
+    //         let db = data;
+
+            // db.items.forEach(element => {
+
                 
-                anchor.className = 'card text-center';
-                anchor = document.getElementById('pokeballs').appendChild(anchor);
 
-                let cardId = document.getElementById(id);
+            //     let id = element.id;
+            //     let name = element.name;
+            //     let category = element.category;
+            //     let image = element.image;
+            //     let overview = element.overview;
+            //     let price = element.price;
+
+            //     let anchor = document.createElement('div');
+            //     anchor.innerHTML = '<div class="card-body" id='+id+'>';
                 
-                let cardImg = document.createElement('img');
-                cardImg.src = 'assets/img/'+image+'';
-                cardImg = cardId.appendChild(cardImg);
+                // if (category == 'Pokeballs') {
+                    
+                //     anchor.className = 'card text-center';
+                //     anchor = document.getElementById('pokeballs').appendChild(anchor);
 
-                let cardName = document.createElement('div');
-                cardName.textContent = name;
-                cardName = cardId.appendChild(cardName);
+                //     let cardId = document.getElementById(id);
+                    
+                //     let cardImg = document.createElement('img');
+                //     cardImg.src = 'assets/img/'+image+'';
+                //     cardImg.alt = name;
+                //     cardImg = cardId.appendChild(cardImg);
 
-                let cardOverview = document.createElement('div');
-                cardOverview.textContent = overview;
-                cardOverview = cardId.appendChild(cardOverview);
+                //     let cardName = document.createElement('div');
+                //     cardName.textContent = name;
+                //     cardName = cardId.appendChild(cardName);
 
-                let cardInput = document.createElement('input');
-                cardInput.placeholder = 'Quantité';
-                cardInput = cardId.appendChild(cardInput);
+                //     let cardOverview = document.createElement('div');
+                //     cardOverview.textContent = overview;
+                //     cardOverview = cardId.appendChild(cardOverview);
 
-                let cardPrice = document.createElement('div');
-                cardPrice.textContent = price+'¥';
-                cardPrice = cardId.appendChild(cardPrice);
+                //     let cardInput = document.createElement('input');
+                //     cardInput.placeholder = 'Quantité';
+                //     cardInput.minLength = 1;
+                //     cardInput.maxLength = 2;
+                //     cardInput = cardId.appendChild(cardInput);
 
-            }
+                //     let cardPrice = document.createElement('div');
+                //     cardPrice.textContent = price+'¥';
+                //     cardPrice = cardId.appendChild(cardPrice);
+
+                // }
             // else if (category == 'Battle items') {
 
             //     let cardImg = document.createElement('img');
@@ -113,8 +146,34 @@ fetch('assets/json/store.json')
             //     cardPrice = cardIds.appendChild(cardPrice);
             // }
 
-    });
-    });
+// });
+//     });
+// else {
+//     document.querySelector('.tab').id = 'landing';
+// }
+// });
+// // Tabs 
+
+
+
+
+
+//     console.log(document.querySelector(element).classList != 'active');
+//     if (document.querySelector(element).classList != 'active') {
+//         document.querySelector('.tab').id = 'landing';
+//     }
+
+
+
+//     // document.querySelector(element).addEventListener('click', () => {
+//     //     if (element.classList != 'active') {
+//     //         element. = 'active';
+
+//     //     }
+//     // });
+// });
+
+
 
 
 // UX
