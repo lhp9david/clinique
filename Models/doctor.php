66461ce78c4bf13dd3143fcfb569d8c1346b0648
MarieSophieGB <mariesophie.gb@gmail.com>
2023-02-28 16:06:11 +0100

    herlpers Database.php

---
+++ Models/doctor.php
@@ -0,0 +1,36 @@
+<?php
+// créer une class Doctor avec les attributs : doctor_id, doctor_lastname, doctor_firstname, doctor_phone, doctor_phone_emergency, doctor_mail, doctor_adress, doctor_photo, doctor_password, specialty_id
+class Doctor
+{
+    private $doctor_id;
+    private $doctor_lastname;
+    private $doctor_firstname;
+    private $doctor_phone;
+    private $doctor_phone_emergency;
+    private $doctor_mail;
+    private $doctor_adress;
+    private $doctor_photo;
+    private $doctor_password;
+    private $specialty_id;
+
+    private object $_pdo;
+
+    // méthode magique pour get les attributs
+    public function __get($attribut)
+    {
+        return $this->$attribut;
+    }
+
+    // méthode magique pour set les attributs
+    public function __set($attribut, $value)
+    {
+        $this->$attribut = $value;
+    }
+
+    // créer un constructeur pour instancier la connexion
+    public function __construct()
+    {
+        $this->_pdo = Database::connect();
+    }
+
+}
