abf746834454effbbf21aceeb3cba7c0dca41f55
David Godard <davidgodard76700@gmail.com>
2023-02-28 15:14:01 +0100

    maj

---
+++ Views/view-patient.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <title>Patient</title>
+</head>
+<body>
+<div class="container">
+    <h1 class="text-center mb-5">Liste des patients</h1>
+<table class="table">
+  <thead>
+    <tr>
+      
+      <th scope="col">Nom</th>
+      <th scope="col">Prénom</th>
+      <th scope="col">Date de naissance</th>
+      <th scope="col">Num Sécu</th>
+      <th scope="col">Tel</th>
+      <th scope="col">Mail</th>
+      <th scope="col">Adresse</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+   
+      <td>Otto</td>
+      <td>Mark</td>
+      <td>20/11/1989</td>
+      <td>1760764568795</td>
+      <td>06.01.02.03.04</td>
+      <td>mark@mail.fr</td>
+      <td>17 rue de Labas</td>
+
+    </tr>
+    <tr>
+      
+      <td>Thornton</td>
+      <td>Jacob</td>
+      <td>20/11/1989</td>
+      <td>1760764568795</td>
+      <td>06.01.02.03.04</td>
+      <td>jacob@mail.fr</td>
+      <td>20 rue d'ici</td>
+
+    </tr>
+  </tbody>
+</table>
+</div>
+</body>
+</html>
