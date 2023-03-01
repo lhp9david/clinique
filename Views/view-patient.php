201e484d181cba04574015205ad0554ded9653e4
David Godard <davidgodard76700@gmail.com>
2023-03-01 10:53:45 +0100

    modal ajout rdv doctor et patient

--- Views/view-patient.php
+++ Views/view-patient.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <title>Patient</title>
-</head>
+<?php include('../includes/head.php'); ?>
 <body>
 <div class="container">
     <h1 class="text-center mb-5">Liste des patients</h1>
--- Views/view-secretary.php
+++ Views/view-secretary.php
@@ -24,7 +24,7 @@
 
                     <h3 class="row">Patients</h3>
                     <div class="row d-flex justify-content-center">
-                        <button type="button" class="btn btn-primary col-10 m-1">Créer un nouveau patient</button>
+                        <button type="button" class="btn btn-primary col-10 m-1" data-bs-toggle="modal" data-bs-target="#patientModal">Créer un nouveau patient</button>
                     </div>
                     <div class="row d-flex justify-content-center">
                         <button type="button" class="btn btn-secondary col-10 m-1">Consulter la liste des patients</button>
@@ -47,7 +47,7 @@
 
                     <h3 class="row">Médecins</h3>
                     <div class="row d-flex justify-content-center">
-                        <button type="button" class="btn btn-primary col-10 m-1">Créer un nouveau médecin</button>
+                        <button type="button" class="btn btn-primary col-10 m-1" data-bs-toggle="modal" data-bs-target="#doctorModal">Créer un nouveau médecin</button>
                     </div>
                     <div class="row d-flex justify-content-center">
                         <button type="button" class="btn btn-secondary col-10 m-1">Consulter la liste des médecins</button>
@@ -70,7 +70,7 @@
 
                     <h3 class="row">Consultations</h3>
                     <div class="row d-flex justify-content-center">
-                        <button type="button" class="btn btn-primary col-10 m-1">Nouvelle consultation</button>
+                        <button type="button" class="btn btn-primary col-10 m-1" data-bs-toggle="modal" data-bs-target="#appointmentModal">Nouvelle consultation</button>
                     </div>
                     <div class="row d-flex justify-content-center">
                         <button type="button" class="btn btn-secondary col-10 m-1">Consulter la liste des consultations</button>
@@ -95,6 +95,168 @@
         </div>
     </div>
     </div>
+
+    <!-- Modal Ajout Medecin -->
+<div class="modal fade" id="doctorModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Ajouter un nouveau medecin</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form action="" method="POST">
+        <div>
+            <label for="name">Nom</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div>
+            <label for="firstname">Prénom</label>
+            <input type="text" name="firstname" id="firstname">
+        </div>
+        <div>
+            <label for="phone">Numéro de téléphone</label>
+            <input type="phone" name="phone" id="phone">
+        </div>
+        <div>
+            <label for="phone">Numéro de téléphone d'urgence</label>
+            <input type="phone" name="phone" id="phone">
+        </div>
+        <div>
+            <label for="mail">Adresse mail</label>
+            <input type="mail" name="mail" id="mail">
+        </div>
+        <div>
+            <label for="firstname">Adresse</label>
+            <input type="text" name="firstname" id="firstname">
+        </div>
+        <div>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div>
+            <label for="password">Confirmer le mot de passe</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div>
+            <select name="speciality" id="speciality">
+                <option value="">Specialité</option>
+                <option value="1">Ophtalmologue</option>
+                <option value="2">Dermatologue</option>
+                <option value="3">Gynécologue</option>
+                <option value="4">Généraliste</option>
+            </select>
+        </div>
+        <div>
+            <!-- upload photo -->
+            <label for="photo">Photo</label>
+            <input type="file" name="photo" id="photo" >
+        </div>
+        <div>
+            <input type="submit">
+        </div>
+    </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+        <button type="button" class="btn btn-primary">Ajouter</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <!-- Modal Ajout patient -->
+    <div class="modal fade" id="patientModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Ajouter un nouveau patient</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form action="" method="POST">
+        <div>
+            <label for="name">Nom</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div>
+            <label for="firstname">Prénom</label>
+            <input type="text" name="firstname" id="firstname">
+        </div>
+        <div>
+            <label for="phone">Numéro de téléphone</label>
+            <input type="phone" name="phone" id="phone">
+        </div>
+        <div>
+            <label for="social">Numéro de sécurité social</label>
+            <input type="text" name="social" id="social">
+        </div>
+        <div>
+            <label for="mail">Adresse mail</label>
+            <input type="mail" name="mail" id="mail">
+        </div>
+        <div>
+            <label for="firstname">Adresse</label>
+            <input type="text" name="firstname" id="firstname">
+        </div>
+
+        <div>
+            <!-- upload photo -->
+            <label for="photo">Photo</label>
+            <input type="file" name="photo" id="photo" >
+        </div>
+        <div>
+            <input type="submit">
+        </div>
+    </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+        <button type="button" class="btn btn-primary">Ajouter</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <!-- Modal Ajout consultation -->
+    <div class="modal fade" id="appointmentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Ajouter une nouvelle consultation</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form action="" method="POST">
+
+        <div>
+            <label for="social">Numéro de sécurité social</label>
+            <input type="text" name="social" id="social">
+        </div>
+        <div>
+            <label for="date">Date</label>
+            <input type="date" name="date" id="date">
+        </div>
+        <div>
+            <label for="hour">Heure</label>
+            <input type="time" name="hour" id="hour">
+        </div>
+
+ 
+        <div>
+            <input type="submit">
+        </div>
+    </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+        <button type="button" class="btn btn-primary">Ajouter</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
